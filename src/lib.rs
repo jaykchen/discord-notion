a65b790767c973f0d1350213686809f794e7e965
Ji Chen <jaykchen@gmail.com>
2023-08-27 21:29:16 -0400

    create a new page

--- src/lib.rs
+++ src/lib.rs
@@ -5,7 +5,10 @@ use http_req::{
     request::{Method, Request},
     uri::Uri,
 };
-use notion_flows::{listen_to_event, notion::models::Page};
+use notion_flows::{
+    listen_to_event,
+    notion::models::{Page, PageCreateRequest},
+};
 use serde::Deserialize;
 use serde_json::json;
 use std::env;
@@ -46,7 +49,38 @@ async fn handle<B: Bot>(bot: &B, em: EventModel) {
             let api = NotionApi::new(notion_token).unwrap();
 
             let page_id = PageId::from_str("21de6521-838e-4003-a964-ca10ec0d9d82").unwrap();
-            match api.get_page(page_id).await {
+
+            let database_id = "1c945dd5-c2d5-48de-bb39-2569fdeca4c3";
+
+            let page = json!({
+                "parent": {
+                    "database_id": database_id
+                },
+                "properties": {
+                    "Name": {
+                        "title": [
+                            {
+                                "text": {
+                                    "content": "Tuscan Kale"
+                                }
+                            }
+                        ]
+                    },
+                    "Description": {
+                        "rich_text": [
+                            {
+                                "text": {
+                                    "content": "A green leafy vegetable."
+                                }
+                            }
+                        ]
+                    }
+                }
+            });
+            let page_json_str = serde_json::to_string(&page).unwrap();
+            let page_request: PageCreateRequest = serde_json::from_str(&page_json_str).unwrap();
+
+            match api.create_page(page_request).await {
                 Ok(page) => {
                     let content = page.properties.title().unwrap_or("no title".into());
                     send_message_to_channel("ik8", "ch_err", content).await;
@@ -56,6 +90,17 @@ async fn handle<B: Bot>(bot: &B, em: EventModel) {
                 }
                 Err(_e) => log::error!("Error: {}", _e),
             };
+
+            // match api.get_page(page_id).await {
+            //     Ok(page) => {
+            //         let content = page.properties.title().unwrap_or("no title".into());
+            //         send_message_to_channel("ik8", "ch_err", content).await;
+            //         _ = client
+            //             .send_message(channel_id.into(), &json!({"content": page.title()}))
+            //             .await
+            //     }
+            //     Err(_e) => log::error!("Error: {}", _e),
+            // };
         }
         _ => {}
     }
